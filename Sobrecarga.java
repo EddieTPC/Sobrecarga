ded4cac514b1c7163615d7151c28746f982ac035
EddieTPC <147268407+EddieTPC@users.noreply.github.com>
2023-10-15 19:33:04 -0700

    Update Sobrecarga.java

--- Sobrecarga.java
+++ Sobrecarga.java
@@ -1,25 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.sobrecarga;
-
-/**
- *
- * @author a
- */
+//Tenemos nuestra clase principal
 public class Sobrecarga {
+    //Creamos una cadena para mostrar en metodoSobrecargado()
     static String cadena="Este método esta sobrecargado";
     
+    //Creamos un método sin parametros.
     public void metodoSobrecargado(){
         System.out.println("Este método esta sobrecargado");
     }
-    public void metodoSobrecargado(String s){
-        System.out.println(s);
+    //Creamos otro metodo con el mismo nombre, pero agreamos un parametro.
+    public void metodoSobrecargado(String cadena){
+        System.out.println(cadena);
     }
     public static void main(String[] args) {
+        //Creamos un objeto de nuestra clase Sobrecarga
        Sobrecarga objeto= new Sobrecarga();
-       
+       //Se muestran ambos metodos con el mismo identificador
        objeto.metodoSobrecargado();
        objeto.metodoSobrecargado(cadena);
     }
